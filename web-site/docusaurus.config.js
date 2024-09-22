f465337c6527976303f8e6ee8664aae7f78e38c5
Hector <eltortkd@outlook.com>
2024-09-22 14:57:18 -0600

    Added docs in sidebar

--- web-site/docusaurus.config.js
+++ web-site/docusaurus.config.js
@@ -84,9 +84,9 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          //{to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/HectorAlonso18/MotionLight',
             label: 'GitHub',
